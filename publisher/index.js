ede6d1e03cb55ffbb3beb9899b72134317e6dd21
Morten Barklund <morten.barklund@gmail.com>
2022-02-09 00:11:06 -0500

    Updated publisher

---
+++ publisher/index.js
@@ -0,0 +1,83 @@
+const glob = require('glob');
+const path = require('path');
+const fs = require('fs');
+const fse = require('fs-extra');
+const cp = require('child_process');
+
+const projectsFolder = path.join(process.cwd(), '_projects');
+if (fs.existsSync(projectsFolder)) {
+  fs.rmSync(projectsFolder,  { recursive: true, force: true });
+}
+fs.mkdirSync(projectsFolder, 0744);
+
+function generatePackage(packageJson) {
+  const content = JSON.parse(packageJson);
+  const rawName = content.name;
+  const chapterId = rawName.slice(2,4);
+  content.name = `cra-template-${rawName}`;
+  delete content.private;
+  content.files = ["template", "template.json"];
+  content.repository = {
+    type: "git",
+    url: "https://github.com/rq2e/rq2e/",
+    directory: `ch${chapterId}/${rawName}`,
+  };
+  content.bugs = { url: "https://github.com/rq2e/rq2e/issues" };
+  content.scripts["clean-files"] = "rm -rf ./template/public ./template/src",
+  content.scripts["copy-files"] = "cp -a ./src/. template/src && cp -a ./public/. template/public",
+  content.scripts["prepublishOnly"] = "yarn clean-files && yarn copy-files"
+  return JSON.stringify(content, null, 2);
+}
+
+function generateTemplate(packageJson) {
+  const { scripts, dependencies } = JSON.parse(packageJson);
+  return JSON.stringify({ scripts, dependencies }, null, 2);
+}
+
+const filesToCopy = [
+  { source: '/public', destination: ['/public', '/template/public'] },
+  { source: '/src', destination: ['/src', '/template/src'] },
+  { source: '/README.md', destination: ['/README.md'] },
+  { source: '/yarn.lock', destination: ['/yarn.lock'] },
+  { source: '/package-lock.json', destination: ['/package-lock.lock'] },
+  { source: '/.gitignore', destination: ['/.gitignore', '/template/gitignore'] }, // Not a typo - no dot required
+  { source: '/.*', destination: ['/<basename>'] },
+  { source: '/package.json', converter: generatePackage, destination: ['/package.json'] },
+  { source: '/package.json', converter: generateTemplate, destination: ['/template.json'] },
+];
+
+function publish(sourceFolder, index) {
+  const progress = `[${index+1}/${projects.length}]`;
+  const projectName = path.basename(sourceFolder);
+  const projectFolder = path.join(projectsFolder, projectName);
+  const templateFolder = path.join(projectFolder, 'template');
+  fs.mkdirSync(projectFolder, 0744);
+  fs.mkdirSync(templateFolder, 0744);
+
+  // Copy files and folder according to the map above
+  filesToCopy.forEach(({ source, destination, converter }) => {
+    glob.sync(path.join(sourceFolder, source)).forEach(sourcePath =>
+      destination.forEach(destinationName => {
+        const destinationPath = path.join(projectFolder, destinationName.replace('<basename>', path.basename(sourcePath)));
+        if (!converter) {
+          fse.copySync(sourcePath, destinationPath);
+        } else {
+          const convertedContent = converter(fs.readFileSync(sourcePath));
+          fs.writeFileSync(destinationPath, convertedContent);
+        }
+      })
+    );
+  });
+
+  // Then publish
+  try {
+    cp.execSync('npm publish 2>&1', { cwd: projectFolder});
+    console.log(`Successfully published "${projectName}" ${progress}`);
+  } catch(e) {
+    console.error(`Skipped publishing "${projectName}" ${progress}`);
+  }
+
+}
+
+const projects = glob.sync('../ch*/rq*');
+projects.forEach(publish);
