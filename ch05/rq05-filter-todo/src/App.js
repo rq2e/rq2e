a2126c564820a759332cfce973e6652e0aea2253
Morten Barklund <morten.barklund@gmail.com>
2022-07-21 18:10:59 +0200

    Fixed a bug in a ch05 example

--- ch05/rq05-filter-todo/src/App.js
+++ ch05/rq05-filter-todo/src/App.js
@@ -1,53 +1,44 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 function markDone(list, index) {
-  return list.map(
-    (item, i) =>
-      i === index
-        ? { ...item, done: true }
-        : item
-  )
+  return list.map((item, i) => (i === index ? { ...item, done: true } : item));
 }
 
-function TodoApplication({initialList}) {
+function TodoApplication({ initialList }) {
   const [todos, setTodos] = useState(initialList);
   const [hideDone, setHideDone] = useState(false);
-  const filteredTodos = hideDone
-    ? todos.filter(({done}) => !done)
-    : todos;
+  const filteredTodos = hideDone ? todos.filter(({ done }) => !done) : todos;
   return (
     <main>
-      <div style={{display: 'flex'}}>
-        <button onClick={() => setHideDone(false)}>
-          Show all
-        </button>
-        <button onClick={() => setHideDone(true)}>
-          Hide done
-        </button>
+      <div style={{ display: "flex" }}>
+        <button onClick={() => setHideDone(false)}>Show all</button>
+        <button onClick={() => setHideDone(true)}>Hide done</button>
       </div>
       {filteredTodos.map((todo, index) => (
         <p key={todo.task}>
-        {todo.done ? (
-          <strike>{todo.task}</strike>
-        ) : (
-          <>
-            {todo.task}
-            <button onClick={() =>
-              setTodos(todos => markDone(todos, index))
-            }>x</button>
-          </>
-        )}
+          {todo.done ? (
+            <strike>{todo.task}</strike>
+          ) : (
+            <>
+              {todo.task}
+              <button
+                onClick={() => setTodos((todos) => markDone(todos, todo.index))}
+              >
+                x
+              </button>
+            </>
+          )}
         </p>
-      ))} 
+      ))}
     </main>
   );
 }
 
 function App() {
   const items = [
-    { task: 'Feed the plants', done: false },
-    { task: 'Water the dishes', done: false },
-    { task: 'Clean the cat', done: false },
+    { task: "Feed the plants", done: false, index: 0 },
+    { task: "Water the dishes", done: false, index: 1 },
+    { task: "Clean the cat", done: false, index: 2 },
   ];
   return <TodoApplication initialList={items} />;
 }
